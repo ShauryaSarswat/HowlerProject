3af9a27c1e230f4df63bbe8ac153e5f37628cf11
Your Name <your_email@example.com>
2024-03-24 18:45:13 +0530

    adding the react cdn

---
+++ ServerLog/BasicReact/app.js
@@ -0,0 +1,14 @@
+const parent = document.getElementById("root");
+
+// using javascript
+
+// const h1 = document.createElement("h1");
+// h1.innerText = "Hi shaurya";
+// parent.appendChild(h1);
+
+
+// using ReactJs
+
+const obj = React;
+
+obj.createElement()
