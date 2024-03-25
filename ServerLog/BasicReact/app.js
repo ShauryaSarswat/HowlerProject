7d48ba65ab80a968ad814f6f41b74c841319484b
Your Name <your_email@example.com>
2024-03-25 18:56:51 +0530

    changes

--- ServerLog/BasicReact/app.js
+++ ServerLog/BasicReact/app.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const parent = document.getElementById("root");
+// const parent = document.getElementById("root");
 
-// using javascript
+// // using javascript
 
-// const h1 = document.createElement("h1");
-// h1.innerText = "Hi shaurya";
-// parent.appendChild(h1);
+// // const h1 = document.createElement("h1");
+// // h1.innerText = "Hi shaurya";
+// // parent.appendChild(h1);
 
 
-// using ReactJs
+// // using ReactJs
 
-const obj = React;
+// const obj = React;
 
-const root = ReactDOM.createRoot(parent);
-const h1 = obj.createElement("h1",{},"Hello shaurya, coming from react");
-const h2 = obj.createElement("h2",{},"this is a supporting div");
-const li = obj.createElement("li",{
-    className: 'li-item',
-    title: 'this is simple as shaurya makes it up'
-},"hello1");
-const li2 = obj.createElement("li",{},"hello32");
-const li3 = obj.createElement("li",{},"hello1");
-const li4 = obj.createElement("li",{},"hello2");
-const li5 = obj.createElement("li",{},"hello3");
-const ol = obj.createElement("ol",{},[li,li2,li3,li4,li5])
-const normalDiv = obj.createElement("div",{},[h1,h2,ol]);
-root.render(normalDiv);
+// const root = ReactDOM.createRoot(parent);
+// const h1 = obj.createElement("h1",{},"Hello shaurya, coming from react");
+// const h2 = obj.createElement("h2",{},"this is a supporting div");
+// const li = obj.createElement("li",{
+//     className: 'li-item',
+//     title: 'this is simple as shaurya makes it up'
+// },"hello1");
+// const li2 = obj.createElement("li",{},"hello32");
+// const li3 = obj.createElement("li",{},"hello1");
+// const li4 = obj.createElement("li",{},"hello2");
+// const li5 = obj.createElement("li",{},"hello3");
+// const ol = obj.createElement("ol",{},[li,li2,li3,li4,li5])
+// const normalDiv = obj.createElement("div",{},[h1,h2,ol]);
+// root.render(normalDiv);
